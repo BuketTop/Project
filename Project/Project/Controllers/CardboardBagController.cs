7fc86283864ed5ae36af4f571550e012ee2aa8af
mervealancay <mervealancay@gmail.com>
2022-12-11 02:02:25 +0300

    Update

---
+++ Project/Project/Controllers/CardboardBagController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class CardboardBagController : Controller
+    {
+        // GET: CardboardBag
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
