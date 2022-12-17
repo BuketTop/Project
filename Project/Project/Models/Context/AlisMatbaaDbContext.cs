2778f18158cdf1bf30317c055c89a572a7b7a4fc
mervealancay <mervealancay@gmail.com>
2022-12-17 21:31:25 +0300

    Login Page Update

---
+++ Project/Project/Models/Context/AlisMatbaaDbContext.cs
@@ -0,0 +1,85 @@
+﻿using Project.Model.Entity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Project.Model.Context
+{
+    public class AlisMatbaaDbContext : DbContext
+    {
+        public AlisMatbaaDbContext() : base("AlisMatbaaDbContext")
+        {
+            Database.SetInitializer<AlisMatbaaDbContext>(new ProfileInitializer());
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    //Creating Custom DB Initializer
+    public class ProfileInitializer : DropCreateDatabaseIfModelChanges<AlisMatbaaDbContext>
+    {
+        protected override void Seed(AlisMatbaaDbContext context)
+        {
+            IList<User> userList = new List<User>();
+            userList.Add(new User()
+            {
+                Name = "Merve",
+                Surname = "Alancay",
+                Mail = "mervealancay@gmail.com",
+                PhoneNumber = "",
+                Password = "202cb962ac59075b964b07152d234b70"
+            });
+
+
+            foreach (User user in userList)
+            {
+                context.User.Add(user);
+            }
+
+
+            IList<OrderStatus> orderStatusList = new List<OrderStatus>();
+            orderStatusList.Add(new OrderStatus()
+            {
+                Name = "Yeni"
+            });
+
+            orderStatusList.Add(new OrderStatus()
+            {
+                Name = "Hazırlanıyor"
+            });
+
+            orderStatusList.Add(new OrderStatus()
+            {
+                Name = "Kargoda"
+            });
+
+            orderStatusList.Add(new OrderStatus()
+            {
+                Name = "Teslim Edildi"
+            });
+
+            orderStatusList.Add(new OrderStatus()
+            {
+                Name = "İptal"
+            });
+
+
+            foreach (OrderStatus orderStatus in orderStatusList)
+            {
+                context.OrderStatus.Add(orderStatus);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
